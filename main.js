7898dc1fbce6a35ab8ffdbed17a49a46a13ea8f0
Lua Jaxuka Morel <68887087+Fraymotiff@users.noreply.github.com>
2023-05-07 00:53:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () =>{
     search.classList.toggle('active');
 }
+
+
+let user = document.querySelector('.user');
+
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+    search.classList.toggle('active');
+}
