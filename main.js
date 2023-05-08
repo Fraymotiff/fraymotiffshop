c5d10015327405ecb23ae18769894fbaf2df8bec
Lua Jaxuka Morel <68887087+Fraymotiff@users.noreply.github.com>
2023-05-08 02:49:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,17 +11,21 @@ let user = document.getElementById('#user').onclick = () =>{
 }
 
 
-let slideIndex = 0;
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000);
 }
